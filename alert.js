ac83112226660bfe1d95b957f552e88e6169ac16
alisa17112006 <alisar17112006@gmail.com>
2026-01-19 21:15:25 +0300

    Вывести числа от 1 до 10

--- alert.js
+++ alert.js
@@ -1,22 +1,5 @@
-// Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
-// Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
-function pow(x, n) {
-  let result = x;
-
-  for (let i = 1; i < n; i++) {
-    result *= x;
-  }
-
-  return result;
-}
-
-let x = prompt("x?", '');
-let n = prompt("n?", '');
-
-if (n >= 1 && n % 1 == 0) {
-  alert( pow(x, n) );
-} else {
-  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+for (let i = 1; i <= 10; i++){
+  console.log(i);
 }
 
  
