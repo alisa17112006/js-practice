6bc96497901e3743a43caa587f31492ae63c384e
alisa17112006 <alisar17112006@gmail.com>
2026-01-19 18:03:56 +0300

    Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.

--- alert.js
+++ alert.js
@@ -1,7 +1,22 @@
-function pow (x, n){
-  for(let i = 1; i <= 100; i++){
-    result = x**n
+// Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
+// Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
   }
-  alert(result)
+
+  return result;
 }
- pow(3,2 )
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n >= 1 && n % 1 == 0) {
+  alert( pow(x, n) );
+} else {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+}
+
+ 
