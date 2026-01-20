beceb739446100e9717e977b101b1eaf9e10bd7d
alisa17112006 <alisar17112006@gmail.com>
2026-01-20 13:03:14 +0300

    Найти максимальное число в массиве

--- alert.js
+++ alert.js
@@ -1,8 +1,9 @@
-let count = 0;
-for (let i = 1; i <= 50; i++){ 
-  if(i % 2 == 0){
-     count++
+let arr = [3, 7, 2, 9, 5];
+let max = arr[0];
+for (let i = 0; i < arr.length; i++){ 
+  if(arr[i] > max){
+     max = arr[i];
   }
 }
-console.log(count)
+console.log(max)
 
