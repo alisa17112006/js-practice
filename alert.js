258dbd937bb038f296e314c103e20baee6543144
alisa17112006 <alisar17112006@gmail.com>
2026-01-26 16:36:36 +0300

    Рамка

--- alert.js
+++ alert.js
@@ -1,13 +1,17 @@
 let rows = 5;
+let cols = 5;
 
-for (let i = rows; i >= 1; i--) {
+for (let i = 1; i <= rows; i++) {
     let row = '';
-    for (let j = 1; j <= i; j++) {
-        row += '#';
+    for (let j = 1; j <= cols; j++) {
+        if(i == 1 || i == rows || j == 1 || j == cols){
+            row += '#';
+        }
+        else {
+            row += ' ';
+        }
+        
     }
     console.log(row);
 }
-
-
-
 
